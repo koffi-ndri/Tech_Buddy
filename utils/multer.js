a159529418dce4e3135b8f35651f9024cd30b2d8
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-05-18 18:38:01 +0000

    "change Uploads folder name"

--- utils/multer.js
+++ utils/multer.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, './public/uploads')
+        cb(null, './public/imageUploads')
     },
     filename:function(req, file, cb){
         cb(null, Date.now() + '-' + file.originalname)
