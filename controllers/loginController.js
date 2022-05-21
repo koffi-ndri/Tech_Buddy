6de5b8b9691d22a269632ae1292e461476c42ad9
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-05-21 21:07:15 +0000

    "Added token"

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,6 @@
 const User = require('../model/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const {loginValidation} = require('../utils/hapiJoi');
 
 module.exports.loginController = async(req, res) =>{
@@ -15,5 +16,9 @@ module.exports.loginController = async(req, res) =>{
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if(!validPass) return res.status(400).send('Invalid password');
 
-    res.send("User Logged in");
+    //Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+
+    //res.send("User Logged in");
 };
