1f8945c04de10bb5d8f504354e3b8777a94c4413
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-05-21 16:04:06 +0000

    "User model created"

---
+++ model/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
