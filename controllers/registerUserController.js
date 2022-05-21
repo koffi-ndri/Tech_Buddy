d2271a29c447ade32c130ad8e559bd2aaa792f6c
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-05-21 19:42:09 +0000

    "User already exist checking added"

--- controllers/registerUserController.js
+++ controllers/registerUserController.js
@@ -6,6 +6,10 @@ module.exports.registerUserController = async(req, res) =>{
     const {error} = schema.validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
+    //checking if the user is already in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists');
+
     //saving user to database
     const user = new User({
         username: req.body.name,
