9004d9538532c51ed4163e1d0eb1c35bc833d747
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-05-23 17:13:15 +0000

    Added test for retrieving videos

---
+++ test/retrieveVideosTest.js
@@ -0,0 +1,23 @@
+const { request } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe("Tech Buddy API", () => {
+    describe('GET /api/retrieveVideos', () =>{
+
+        it('it should not GET any images due to unauthorized access', (done) => {
+            chai.request(server)
+                .get('/api/retrieveVideos')
+                .end((err, response) =>{
+                    response.should.have.status(401);
+                    response.text.should.be.eq("Access Denied");
+                    done();
+                });
+        });
+    });
+});
