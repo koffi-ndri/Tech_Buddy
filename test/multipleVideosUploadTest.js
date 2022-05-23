65343c5a4a4b716b5ef5f5f0c53b00fec2ce93e0
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-05-23 17:02:30 +0000

    "Added test for multiple video upload"

---
+++ test/multipleVideosUploadTest.js
@@ -0,0 +1,23 @@
+const { request } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe("Tech Buddy API", () => {
+    describe('POST /api/singleVideoUpload', () =>{
+
+        it('it should not POST any videos due to unauthorized access', (done) => {
+            chai.request(server)
+                .post('/api/singleVideoUpload')
+                .end((err, response) =>{
+                    response.should.have.status(401);
+                    response.text.should.be.eq("Access Denied");
+                    done();
+                });
+        });
+    });
+});
