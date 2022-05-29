a0a4c4fa1d5dea7f2901785a2cc836fd221a265e
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-05-29 18:20:49 +0000

    "login test updated"

--- test/loginTest.js
+++ test/loginTest.js
@@ -24,15 +24,5 @@ describe("Tech Buddy API", () => {
                     return done();
                 });
         });
-
-        // it('it should not GET any images due to unauthorized access', (done) => {
-        //     chai.request(server)
-        //         .get('/api/retrieveImages')
-        //         .end((err, response) =>{
-        //             response.should.have.status(401);
-        //             response.text.should.be.eq("Access Denied");
-        //             done();
-        //         });
-        // });
     });
 });
