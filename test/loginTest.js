10f51f3cb8c85b9560a37ecd691a29daa64624f0
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-07 13:28:02 +0000

    Solving deployment problem

--- test/loginTest.js
+++ test/loginTest.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
-// const User = require('../model/User');
+
 chai.should();
 
 chai.use(chaiHttp);
