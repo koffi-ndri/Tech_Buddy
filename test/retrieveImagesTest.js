9f9e8f350795a9c04145603d66daad19f8ca0af1
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-07 14:00:16 +0000

    "Some lines of codes removed"

--- test/retrieveImagesTest.js
+++ test/retrieveImagesTest.js
@@ -2,22 +2,11 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
 const {userToken} = require('../utils/token');
-//const User = require('../model/User');
+
 chai.should();
 
 chai.use(chaiHttp);
 
-//let user;
-//let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mjg5NDM0YjY0MmMwMDcwNWM4NmRkM2EiLCJpYXQiOjE2NTMxNjY5NjN9.sr0PZfGQBIotZ7gb8v9EOhHevJIEd3RlvGtseq_kVyA';
-    // user = User.find({}, (err, userlist) =>{
-    //     if(err){
-    //         return;
-    //     }else{
-    //         return userlist[0];
-    //     };
-    // });
-
-    //token = userToken(user.email);
 
 describe("Tech Buddy API", () => {
     describe('GET /api/retrieve/images', async() =>{
