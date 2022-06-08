7de44e25796900c46f5b93a00413a9d210580e30
koffi-ndri <87645176+koffi-ndri@users.noreply.github.com>
2022-06-08 12:12:51 +0000

    "cookie parser removed"

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 dotenv.config();
